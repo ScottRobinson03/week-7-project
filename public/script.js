3aa33514a85c9e78e0eb2971194f37ce220ea229
Scott Robinson <scott.robinson2@sky.uk>
2022-11-15 17:41:20 +0000

    Add base frontend for messaging app

---
+++ public/script.js
@@ -0,0 +1,30 @@
+const messageInput = document.getElementById("message-content");
+const submitButton = document.getElementById("submit-message-btn");
+const messageUl = document.getElementById("message-display");
+
+submitButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    displayMessage(messageInput.value);
+});
+
+function displayMessage(messageContent) {
+    const li = document.createElement("li");
+    
+    const div = document.createElement("div");
+    div.classList.add("message-container");
+
+    const pUsername = document.createElement("p");
+    pUsername.classList.add("username");
+    pUsername.innerText = "Anonymous";
+    
+    const pMessage = document.createElement("p");
+    pMessage.classList.add("message-content");
+    pMessage.innerText = messageContent;
+
+    div.append(pUsername, pMessage);
+    li.append(div);
+    messageUl.append(li);
+
+    li.scrollIntoView();
+    messageInput.value = "";
+}
--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 
 const app = express();
 app.use(express.json());
-
+app.use("/", express.static("public"));
 
 if (require.main === module) {
     app.listen(5001, () => {
