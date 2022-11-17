a9ca8e2edaa15bc4277b21c1c2dfac2664d9bc84
Scott Robinson <scott.robinson2@sky.uk>
2022-11-17 21:43:47 +0000

    Remove redundant comment

--- src/routes/home.js
+++ src/routes/home.js
@@ -8,8 +8,5 @@ router.use("/home", verifyJWT_MW, express.static("public/home"));
 router.get("/", (_, resp) => {
     resp.redirect("/home");
 });
-// router.get("/", verifyJWT_MW, (_, res) => {
-//     res.json({success: true, data: "Example data"});
-// });
 
 module.exports = router;
